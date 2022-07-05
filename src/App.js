f859832bc8caea47d3f8afab5491f54d3521ed06
Gagan Preet <gaganpreet.dev@gmail.com>
2022-07-05 23:17:40 +0530

    add searchbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./Main"
+import Search from "./Search";
+import React from "react"
 
 function App() {
+  const fetchData = () => {
+    return fetch(`https://api.github.com/users/${"igaganpreet"}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data.name));
+  }
+  React.useEffect(() => {
+    fetchData()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search />
+      <Main />
     </div>
   );
 }
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import {BiSearchAlt} from "react-icons/bi"
+export default function Search() {
+    return (
+      <div className="Search">
+        <div className="searchBar">
+            <input className="searchInput" placeholder="Enter Name Here"></input>
+            <button className="searchButton">Search</button>
+        </div>
+      </div>
+    );
+  }
+  
