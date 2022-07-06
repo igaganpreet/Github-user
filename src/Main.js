858cea38a17b4a65f98ae863ca0033c6d21c7270
Gagan Preet <gaganpreet.dev@gmail.com>
2022-07-07 00:42:37 +0530

    complete user search and display

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,28 @@
 import {AiFillGithub} from "react-icons/ai"
 import {IoLocationSharp} from "react-icons/io5"
+import {BiLink} from "react-icons/bi"
+import {FiTwitter, FiUsers} from "react-icons/fi"
+import {MdPeopleAlt} from "react-icons/md"
+import {BsDot} from "react-icons/bs"
 
 export default function Main(props) {
-console.log(props.info)
+let user=props.info
     return (
       <div className="Main">
         <img alt="profileImage" className="userImage" src={props.info.avatar_url} ></img>
-        <div className="mainData">
-          <h1>{props.info.name}</h1>
-          <h3>{props.info.login}</h3>
-          <p>{props.info.bio}</p>
-          <p><AiFillGithub />{props.info.html_url}</p>
-          <p><IoLocationSharp />{props.info.location}</p>
+        
+        <div className="mainUserData">
+          <h1 className="name">{user.name}</h1>
+          <h3 className="username">{user.login}</h3>
+          <p className="bio">{user.bio}</p>
+          <p className="userDataDesc"><AiFillGithub className="userDataIcon" />{user.html_url}</p>
+          {user.location ? <p className="userDataDesc"><IoLocationSharp className="userDataIcon" />{user.location}</p>:""}
+          {user.blog ? <p className="userDataDesc"><BiLink className="userDataIcon" />{user.blog}</p> : ""}
+          {user.twitter_username ? <p className="userDataDesc"><FiTwitter className="userDataIcon" />{user.twitter_username}</p>: ""}
+          <p className="userDataDesc"><MdPeopleAlt className="userDataIcon" />{user.followers} followers <BsDot /> {user.following} following </p>
+          <button className="showRepo">Show Repositories</button>
         </div>
+
       </div>
     );
   }
