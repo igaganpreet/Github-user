7dcebbd1ed3a615afd1c4c59041958faf65330ae
Gagan Preet <gaganpreet.dev@gmail.com>
2023-03-16 01:36:07 +0530

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,7 @@ export default function Search(props) {
     return (
       <div className="Search" style={{marginTop:(props.isRepoButtonClicked) ? "2%" : "6%"}}>
         <div className="searchBar">
-            <input className="searchInput" placeholder="Enter Name Here" onChange={props.getSearchVal}></input>
+            <input className="searchInput" placeholder="Enter user name here" onChange={props.getSearchVal}></input>
             <button className="searchButton" onClick={props.searchUser}>Search</button>
         </div>
       </div>
